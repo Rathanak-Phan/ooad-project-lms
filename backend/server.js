9d1486a6e18eccdabb80375ba7aa3d0afaadb3d6
Rathanak <studyingalaxy123@gmail.com>
2025-09-11 01:46:36 +0700

    Update commite

--- backend/server.js
+++ backend/server.js
@@ -3,13 +3,32 @@ import express from 'express';
 const app = express();
 
 app.get('/', (req, res) => {
-  console.log('Request from : ', req.ip);
-  res.send("Hello World");
-  res.sendStatus(200);
-  res.send("Hello World 2");
-})
+//   res.send('Hello World!');
+//   res.send('Hello World! from Rathanak');
+    res.json({ 
+        id : 1,
+        name : "Rathanak",
+        age : 24,
+        email : "example@gmail.com",
+        address : "Phnom Penh, Cambodia",
+        phone : "012345678",
+        skills : ["JavaScript", "React", "Node.js"],
+        experience : [
+            {
+                company : "ABC Company",
+                role : "Frontend Developer",
+                duration : "2 years"
+            },
+            {
+                company : "XYZ Company",
+                role : "Backend Developer",
+                duration : "1 year"
+            }
+        ],
+    });
+        
+});
 
 app.listen(3000, () => {
-  console.log('Server is running on port 3000');
-  
-})
+  console.log('Server is running on http://localhost:3000');
+});
