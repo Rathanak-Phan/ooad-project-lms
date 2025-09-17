5c8d979b077fb0c7363bf232943f1a8f3aca7007
Rathanak <studyingalaxy123@gmail.com>
2025-09-18 01:40:41 +0700

    how to get method

--- backend/server.js
+++ backend/server.js
@@ -1,34 +1,35 @@
-import express from 'express';
+import express from "express";
 
 const app = express();
 
-app.get('/', (req, res) => {
-//   res.send('Hello World!');
-//   res.send('Hello World! from Rathanak');
-    res.json({ 
-        id : 1,
-        name : "Rathanak",
-        age : 24,
-        email : "example@gmail.com",
-        address : "Phnom Penh, Cambodia",
-        phone : "012345678",
-        skills : ["JavaScript", "React", "Node.js"],
-        experience : [
-            {
-                company : "ABC Company",
-                role : "Frontend Developer",
-                duration : "2 years"
-            },
-            {
-                company : "XYZ Company",
-                role : "Backend Developer",
-                duration : "1 year"
-            }
-        ],
-    });
-        
+app.get("/", (req, res) => {
+  //   res.send('Hello World!');
+  //   res.send('Hello World! from Rathanak');
+  // res.json({
+  //     id : 1,
+  //     name : "Rathanak",
+  //     age : 24,
+  //     email : "example@gmail.com",
+  //     address : "Phnom Penh, Cambodia",
+  //     phone : "012345678",
+  //     skills : ["JavaScript", "React", "Node.js"],
+  // });
+
+    res.send(`
+        <script>
+            window.location.href = "https://rathanak-phan.vercel.app/";
+        </script>
+    `);
 });
 
+// app.get("/about", (req, res) =>{
+//     res.send(`
+//         <script>
+//             window.location.href = "https://rathanak-phan.vercel.app/about";
+//         </script>
+//     `);
+// });
+
 app.listen(3000, () => {
-  console.log('Server is running on http://localhost:3000');
+  console.log("Server is running on http://localhost:3000");
 });
